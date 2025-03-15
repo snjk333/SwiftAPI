0cc4c90eb869b3d4823fc9575263dada0f9cbefb
= <=>
2025-03-15 11:11:51 +0100

    init commit

---
+++ src/main/java/com/oleksandr/remitly/swiftapi/SwiftApiApplication.java
@@ -0,0 +1,13 @@
+package com.oleksandr.remitly.swiftapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwiftApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwiftApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oleksandr/remitly/swiftapi/SwiftApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oleksandr.remitly.swiftapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwiftApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
