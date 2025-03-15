40f89d3a96e3f5e5cd3100f57f559cecec89616a
= <=>
2025-03-15 11:32:19 +0100

    Add Country entity with jakarta annotations

---
+++ src/main/java/com/oleksandr/remitly/swiftapi/entity/Country.java
@@ -0,0 +1,63 @@
+package com.oleksandr.remitly.swiftapi.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "countries")
+public class Country {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "iso_code", nullable = false, unique = true)
+    private String isoCode;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "timezone_id", nullable = false)
+    private Timezone timezone;
+
+    public Country() {
+    }
+
+    public Country(String isoCode, String name, Timezone timezone) {
+        this.isoCode = isoCode;
+        this.name = name;
+        this.timezone = timezone;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getIsoCode() {
+        return isoCode;
+    }
+
+    public void setIsoCode(String isoCode) {
+        this.isoCode = isoCode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Timezone getTimezone() {
+        return timezone;
+    }
+
+    public void setTimezone(Timezone timezone) {
+        this.timezone = timezone;
+    }
+}
+
