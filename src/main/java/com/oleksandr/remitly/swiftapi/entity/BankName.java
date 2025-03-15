e243964112947c494f6c5611227bab07cf246c22
= <=>
2025-03-15 11:37:58 +0100

    Add BankName entity with jakarta annotations for DB connection

---
+++ src/main/java/com/oleksandr/remitly/swiftapi/entity/BankName.java
@@ -0,0 +1,44 @@
+package com.oleksandr.remitly.swiftapi.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "bank_names")
+public class BankName {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+
+    @ManyToOne
+    @JoinColumn(name = "country_id", nullable = false)
+    private Country country;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Country getCountry() {
+        return country;
+    }
+
+    public void setCountry(Country country) {
+        this.country = country;
+    }
+}
