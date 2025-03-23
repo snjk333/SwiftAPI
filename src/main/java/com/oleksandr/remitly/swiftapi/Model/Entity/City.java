a4f2d7b102d334059e351c69849ec7acb1985a56
= <=>
2025-03-23 10:01:58 +0100

    Add SwiftCodeRepository

--- src/main/java/com/oleksandr/remitly/swiftapi/Model/Entity/City.java
+++ src/main/java/com/oleksandr/remitly/swiftapi/Model/Entity/City.java
@@ -1,8 +1,7 @@
 package com.oleksandr.remitly.swiftapi.Model.Entity;
 
 import jakarta.persistence.*;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.List;
 
---
+++ src/main/java/com/oleksandr/remitly/swiftapi/Repository/SwiftCodeRepository.java
@@ -0,0 +1,9 @@
+package com.oleksandr.remitly.swiftapi.Repository;
+
+import com.oleksandr.remitly.swiftapi.Model.Entity.SwiftCode;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SwiftCodeRepository extends JpaRepository<SwiftCode, Integer>{
+
+
+}
