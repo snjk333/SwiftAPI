8ee11610f2c785e16bf9d4174d47b40c105fa87e
= <=>
2025-03-23 11:13:05 +0100

    Add getSwiftCodesByISO method

--- src/main/java/com/oleksandr/remitly/swiftapi/Service/SwiftServiceImpl.java
+++ src/main/java/com/oleksandr/remitly/swiftapi/Service/SwiftServiceImpl.java
@@ -34,7 +34,36 @@ public class SwiftServiceImpl implements SwiftService{
 
     @Override
     public CountrySwiftCodesResponseDTO getSwiftCodesByISO(String isoCode) {
-       return null;
+        List<SwiftCode> swiftCodeList = swiftCodeRepository.findByBank_Country_isoCode(isoCode);
+        // we take list of swift codes by ISO2 code
+        if (swiftCodeList.isEmpty()) {
+            return new CountrySwiftCodesResponseDTO(isoCode, "Unknown", Collections.emptyList());
+        }
+        // if we don't have any codes -> return empty list
+
+        // we take info step by step
+        // take county at first
+        Country country = swiftCodeList.get(0).getBank().getCountry();
+
+        //take ISO2 code from country
+        String countryISO = country.getIsoCode();
+
+        //take name from country
+        String countryName = country.getName();
+
+        //map List of DTO objects from swiftCodeList
+        List<SwiftCodeDetailsDTO> details = swiftCodeList.stream()
+                .map(sc -> new SwiftCodeDetailsDTO(
+                        sc.getAddress(),
+                        sc.getBank().getName(),
+                        countryISO,
+                        sc.isHeadquarter(),
+                        sc.getSwiftCode()))
+                .collect(Collectors.toList());
+
+        // use all info to return
+        return new CountrySwiftCodesResponseDTO(countryISO, countryName, details);
+
     }
 
     @Override
