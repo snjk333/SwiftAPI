ef233015121e3f1a6e1e1cd9697323675e7af57f
= <=>
2025-03-23 11:11:58 +0100

    Updated SwiftService interface. Changed method definition

--- src/main/java/com/oleksandr/remitly/swiftapi/Service/SwiftService.java
+++ src/main/java/com/oleksandr/remitly/swiftapi/Service/SwiftService.java
@@ -1,14 +1,16 @@
 package com.oleksandr.remitly.swiftapi.Service;
 
+import com.oleksandr.remitly.swiftapi.Model.DTO.BankResponseDTO;
+import com.oleksandr.remitly.swiftapi.Model.DTO.CountrySwiftCodesResponseDTO;
 import com.oleksandr.remitly.swiftapi.Model.DTO.SwiftCodeRequestDTO;
 import com.oleksandr.remitly.swiftapi.Model.Entity.SwiftCode;
 
 import java.util.List;
 
 public interface SwiftService {
-    List<SwiftCode> getSwiftCodeDetails(String swiftCode); //endpoint 1
+    BankResponseDTO getSwiftCodeDetails(String swiftCode); //endpoint 1
 
-    List<SwiftCode> getSwiftCodesByISO(String swiftCode); //endpoint 2
+    CountrySwiftCodesResponseDTO getSwiftCodesByISO(String swiftCode); //endpoint 2
 
     void addSwiftCode(SwiftCodeRequestDTO swiftCode); //endpoint 3
 
