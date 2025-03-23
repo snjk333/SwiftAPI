0290d46f695e8b16b3ef3aa50b454a91cdfb1577
= <=>
2025-03-23 10:33:24 +0100

    Add custom method to SwiftCodeRepository interface using special regex method

--- src/main/java/com/oleksandr/remitly/swiftapi/Repository/SwiftCodeRepository.java
+++ src/main/java/com/oleksandr/remitly/swiftapi/Repository/SwiftCodeRepository.java
@@ -2,8 +2,20 @@ package com.oleksandr.remitly.swiftapi.Repository;
 
 import com.oleksandr.remitly.swiftapi.Model.Entity.SwiftCode;
 import org.springframework.data.jpa.repository.JpaRepository;
+import  com.oleksandr.remitly.swiftapi.Model.Entity.BankName;
+import java.util.List;
+import java.util.Optional;
 
 public interface SwiftCodeRepository extends JpaRepository<SwiftCode, Integer>{
 
+    //endpoint 1
+    Optional<SwiftCode> findBySwiftCode(String swiftCode); // I will find optional swift code, take bank
+    List<SwiftCode> findByBank(BankName bank); // And use bank to take all swift codes
+
+    //endpoint 2
+    List<SwiftCode> findByBank_Country_isoCode(String isoCode); // Bank -> Country - isoCode == isoCode.
+
+    //endpoint 4
+    void deleteBySwiftCode(String swiftCode); //delete row with this swift code
 
 }
