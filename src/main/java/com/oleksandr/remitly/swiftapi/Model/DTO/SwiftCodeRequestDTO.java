5fcf981841e695167fbd9120d440a3ed3082ea46
= <=>
2025-03-23 10:38:27 +0100

    Add SwiftCodeRequestDTO

---
+++ src/main/java/com/oleksandr/remitly/swiftapi/Model/DTO/SwiftCodeRequestDTO.java
@@ -0,0 +1,17 @@
+package com.oleksandr.remitly.swiftapi.Model.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SwiftCodeRequestDTO {
+    private String address;
+    private String bankName;
+    private String countryISO2;
+    private String countryName;
+    private boolean isHeadquarter;
+    private String swiftCode;
+}
