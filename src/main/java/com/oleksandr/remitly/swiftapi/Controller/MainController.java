2eee732127b398ff9c5b6d30134c56871f5a8805
= <=>
2025-03-27 15:07:22 +0100

    Add empty controller

---
+++ src/main/java/com/oleksandr/remitly/swiftapi/Controller/MainController.java
@@ -0,0 +1,14 @@
+package com.oleksandr.remitly.swiftapi.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1")
+public class MainController {
+
+
+
+
+}
